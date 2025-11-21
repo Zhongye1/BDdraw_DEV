472fd786eec565d8996804f7437c76988b99277c
Zhongye1 <2760913192@qq.com>
2025-11-21 11:26:50 +0800

    feat: 添加 arco-design，更新helmet为 react-helmet-async，删除余项

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import path from 'path'
+import { vitePluginForArco } from '@arco-plugins/vite-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
     svgr({
       exportAsDefault: true,
     }),
+    vitePluginForArco(),
     react(),
   ],
   resolve: {
